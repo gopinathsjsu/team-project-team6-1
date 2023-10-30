0b78e31cb750198d5b0931b1f3e36d6603c44e79
Sayali Bayaskar <sayalibayaskar@Sayalis-MacBook-Air.local>
2023-10-29 17:32:05 -0700

    login api changes

--- dbconnector.py
+++ dbconnector.py
@@ -1,10 +1,14 @@
 
 from connection import connection
 import psycopg2
+import psycopg2.extras
 
 conn = None
 # read connection parameters
 params = connection()
+ 
+
+'''
 
 class CursorByName():
     def __init__(self, cursor):
@@ -16,32 +20,36 @@ class CursorByName():
     def __next__(self):
         row = self._cursor.__next__()
         return { description[0]: row[col] for col, description in enumerate(self._cursor.description) }
-    
 
-def checkLoginCredentials(requestdata):
-    username = requestdata["username"]
-    password = requestdata["password"]
-    print(username)
-    print(password)
+'''
+
+def checkLoginCredentials(username, password):
+    
     data = []
     try:
         #establish connection
         with psycopg2.connect(**params) as conn:
 
             # create a cursor 
-            with conn.cursor() as cur:
+            with conn.cursor(cursor_factory = psycopg2.extras.RealDictCursor) as cur:
 
                 #write query
-                query = f'SELECT username FROM usertable WHERE username = %s AND userpassword = %s'
+                query = f'''SELECT username, membership.* FROM usertable
+                        INNER JOIN (
+                            SELECT * from usermembership
+                        )membership
+                        on usertable.userid = membership.userid
+                        WHERE username = %s AND userpassword = %s'''
                 #query = f'SELECT * FROM usertable'
 
                 #fetch data from server
                 cur.execute(query, (username, password)) 
-
+                '''
                 for row in CursorByName(cur):
                     data.append(row)
+                '''
 
-                #data = cur.fetchall()
+                data = cur.fetchall()
                 print(data)
                 print('successfully read in data')
     except (Exception, psycopg2.DatabaseError) as error:
--- main.py
+++ main.py
@@ -1,16 +1,24 @@
 from datetime import date
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, render_template
 import dbconnector as dbc
 
 
 app =Flask(__name__)
 
-@app.route("/login",methods=["POST"])
+@app.route('/login')
+def login():
+   return render_template('Login.html')
+
+@app.route("/signin",methods=["POST"])
 def signin():
     requestdata = request.get_json()
 
+    username = requestdata["username"]
+    password = requestdata["password"]
+
     #use database connector object to connect to database and retrieve data
-    responsedata = dbc.checkLoginCredentials(requestdata)
+    responsedata = dbc.checkLoginCredentials(username, password)
+    print(responsedata)
     if "error" in responsedata[0]:
         return responsedata, 400
     return responsedata, 200
