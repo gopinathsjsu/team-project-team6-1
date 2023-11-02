512136d9e86e26b257587722be937cbae7fe411b
Sita Lakshmi <aishwaryashankar17@gmail.com>
2023-11-01 17:48:13 -0700

    added dbconnector function for registering user and adding their membership record and modified register/sign-up api

--- main.py
+++ main.py
@@ -7,9 +7,15 @@ import psycopg2
 
 app = Flask(__name__)
 
+'''
+@app.route('/')
+def home():
+    return render_template('registration.html')
+'''
+
 @app.route("/signin",methods=["POST"])
 def signin():
-
+    '''
     headers = request.headers
     bearer = headers.get('Authorization')    # Bearer YourTokenHere
     if(bearer is None):
@@ -19,7 +25,7 @@ def signin():
 
     if token != "xyz-secret-key":
         return "Unauthorised user", 401
-
+    '''
     requestdata = request.get_json()
 
     username = requestdata["username"]
@@ -34,7 +40,7 @@ def signin():
 
 @app.route("/currentmovies",methods=["GET"])
 def currentmovies():
-
+    '''
     headers = request.headers
     bearer = headers.get('Authorization')    # Bearer YourTokenHere
     if(bearer is None):
@@ -44,39 +50,35 @@ def currentmovies():
 
     if token != "xyz-secret-key":
         return "Unauthorised user", 401
-    
+    '''
+
     responsedata = dbc.getCurrentMovies()
     #print(responsedata)
     if "error" in responsedata[0]:
         return responsedata, 400
     return responsedata, 200
+    
 
-
-try:
-    db = psycopg2.connect("dbname=projectdb user=postgres password=postgres")
-    cursor = db.cursor()
-except:
-    print('Could not connect to the database.')
 
 # api to register a user and add their info to database
-@app.route('/register', methods=['POST'])
+@app.route('/signup', methods=['POST'])
 def register():
-    result = json.dumps(request.form)
-    result = json.loads(result) 
+   
+    json_data = jsonify(request.form)
+    result = json_data.get_json()
     fullname = result['name']
     username = result['username']
     password = result['password']
     phoneno = result['phone_no']
     address = result['address']
     role = 'User'
-    sql = '''INSERT INTO usertable (userid, fullname, phonenumber, address, username, userpassword, userrole) VALUES (%s, %s, %s, %s, %s, %s, %s);'''
-    cursor.execute('''select max(userid) from usertable;''')
-    max_id = cursor.fetchone()[0]
-    print(max_id)
-    print(type(max_id))
-    cursor.execute(sql, (max_id + 1, fullname, phoneno, address, username, password, role))
-    db.commit()
-    return result
+    response= dbc.registeruser(fullname, phoneno, address, username,password, role)
+    print("response:",response)
+    data=response.get_json()
+    data = dbc.registerUserMembership(username)
+    print("Data : ",data)
+    print(type(data))
+    return data
 
 if __name__ == '__main__':
     app.run()
