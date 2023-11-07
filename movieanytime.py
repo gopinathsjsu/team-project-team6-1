227143ee53b17055e1df53c44550568822f4bc3a
SayaliVB <svbayaskar@gmail.com>
2023-11-07 12:09:02 -0800

    added session variables
    added session variable and updated the signin.html for disabling javascript. Added routing url for login  to call api

--- movieanytime.py
+++ movieanytime.py
@@ -1,10 +1,29 @@
-from flask import Flask, jsonify, request, render_template
+from flask import Flask, redirect, session, request, render_template, url_for
 import json
 import requests
 
 
 
 app = Flask(__name__)
+app.secret_key = 'fwe_5HvBK=9CvoqSD87xm'
+
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    if request.method == "POST":
+        jsonrequest = json.dumps(request.form)
+        r = requests.post('http://127.0.0.1:5000/signin', data=jsonrequest, headers= {'Content-Type': 'application/json'})
+        #username = session['username']
+        response = json.loads(r.text)
+        if r.status_code == 200:
+            session['username']=response[0]['username']
+            session['ispremium']=response[0]['ispremium']
+            session['userid']=response[0]['userid']
+            return redirect(url_for('current_movies'))
+        else:
+            #show error message, maybe send a variable here to display in html with jinja
+            return render_template('signin.html')
+    return render_template('signin.html')
+
 
 @app.route('/register', methods=['POST', 'GET'])
 def register():
@@ -21,16 +40,15 @@ def register():
 @app.route('/', methods=['GET'])
 def current_movies():
    if request.method == "GET":
+        #use session variables
+        print(session['username'])
         current_movies = requests.get('http://127.0.0.1:5000/currentmovies')
-        print(current_movies.text)
+        #print(current_movies.text)
         current_movies_json=json.loads(current_movies.text)
-        featuring_movies=current_movies_json[:4]
-        
         upcoming_movies = requests.get('http://127.0.0.1:5000/upcomingmovies')
-        print(upcoming_movies.text)
+        #print(upcoming_movies.text)
         upcoming_movies_json=json.loads(upcoming_movies.text)
-        
-   return render_template("testhome.html",movies = current_movies_json,Featuring_movies=featuring_movies)
+   return render_template("testhome.html",movies = current_movies_json)
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1',port=5001)
