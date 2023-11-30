f096ea48e08fb3829319eef69c4acc24e08a6e6f
SaiPranaviKurapati <saipranavi.kurapati@sjsu.edu>
2023-11-30 10:09:12 -0800

    Integration of bookmovie and seatselection

--- main.py
+++ main.py
@@ -3,9 +3,10 @@ import json
 from flask import Flask, request, jsonify
 import dbconnector as dbc
 import rsa
+from flask_cors import CORS
 
 app = Flask(__name__)
-
+CORS(app)
 
 # api to check username and password for login
 @app.route("/signin",methods=["POST"])
